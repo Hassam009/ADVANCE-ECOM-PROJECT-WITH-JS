235b2e44079b693ede431ff5dfc13ce0e1418e33
Hassam Amer <hasideveloper@gmail.com>
2024-04-16 23:51:38 +0500

    Made another file in which basically we call the products and shows throught query selector

---
+++ MY-Ecommerce-Project-with-JS/homeProductsCards.js
@@ -0,0 +1,12 @@
+
+
+const productContainer=document.querySelector(".productContainer");
+
+const productTemplate=document.querySelector(".productTemplate")
+
+
+export const showProductContainer=(products)=>{
+if(!products){
+    return false;
+}
+}
