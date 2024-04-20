02530be1a0eb48d596d64e3f4144848bca44fec2
Hassam Amer <hasideveloper@gmail.com>
2024-04-20 14:40:02 +0500

    Now almost done with increment and decrement

--- MY-Ecommerce-Project-with-JS/homeQuantityToggle.js
+++ MY-Ecommerce-Project-with-JS/homeQuantityToggle.js
@@ -1,6 +1,27 @@
 export const homeQuantityToggle=(Event,id,stock)=>{
 
- const currentProductElement=document.querySelector(`card${id}`)
- 
+ const currentProductElement=document.querySelector(`#card${id}`)
+// console.log(currentProductElement)
 
-} 
+const productQuantity=currentProductElement.querySelector('.prdocutQuantity');
+
+let quantity=parseInt(productQuantity.getAttribute('data-quantity')) || 1;
+
+if(Event.target.className==='cartIncrement'){
+    if(quantity<stock){
+        quantity+=1;
+    }else if(quantity===stock){
+        quantity=stock;
+    }
+}
+
+if(Event.target.className==='cartDecrement'){
+    if(quantity>1){
+        quantity-=1;
+    }
+}
+
+
+productQuantity.innerText=quantity;
+productQuantity.getAttribute("data-quantity", quantity);
+}   
